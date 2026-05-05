b3d950f8b292de17530e1b4db6b0855d69101b87
tehillasebrow <tehillasebrow123@gmail.com>
2026-05-04 22:13:45 -0400

    Update TaskRegistry.java

--- src/main/java/edu/touro/mcon364/concurrency/lesson1/homework/TaskRegistry.java
+++ src/main/java/edu/touro/mcon364/concurrency/lesson1/homework/TaskRegistry.java
@@ -17,32 +17,28 @@ import java.util.Optional;
  * - size(): return current number of tasks
  * - snapshot(): return a defensive copy that callers cannot use to mutate internal state
  */
-public  class TaskRegistry {
+public class TaskRegistry {
 
-    private final  Map<Integer, Task> tasks = new HashMap<>();
+    private final Map<Integer, Task> tasks = new HashMap<>();
 
     public synchronized void add(Task task) {
-        // TODO: make thread-safe
         tasks.put(task.id(), task);
     }
 
     public synchronized Optional<Task> findById(int id) {
-        // TODO: make thread-safe
         return Optional.ofNullable(tasks.get(id));
     }
 
     public synchronized Optional<Task> remove(int id) {
-        // TODO: make thread-safe
         return Optional.ofNullable(tasks.remove(id));
     }
 
     public synchronized int size() {
-        // TODO: make thread-safe
         return tasks.size();
     }
 
     public synchronized Map<Integer, Task> snapshot() {
-        // TODO: return a defensive copy safely
+        // Defensive, unmodifiable copy
         return Map.copyOf(tasks);
     }
 }
